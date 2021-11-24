e11b8468d652bb956de2ad05169acfbc9c46431a
Brian Diaz <bidiaz101@gmail.com>
2021-11-24 16:09:00 -0600

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.new(item_params)
+    item[:user_id] = params[:user_id]
+    item.save
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def record_not_found
+    render json: { error: "Record not found" }, status: 404
   end
 
 end
